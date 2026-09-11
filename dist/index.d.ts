1dfc255634232855a61639d28d2ee395d44d8ed7
Thomas Vu <thomasjvu@gmail.com>
2026-09-11 17:59:50 -0400

    Sync @phantasy/plugin-slack (source-extracted) from monorepo export

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+export declare class SlackPlugin {
+  name: string;
+  version: string;
+  description: string;
+  getTools(): never[];
+}
+export default SlackPlugin;
---
+++ dist/index.js
@@ -0,0 +1,9 @@
+export class SlackPlugin {
+  name = "slack";
+  version = "0.1.17-beta";
+  description = "Slack messaging integration plugin for Phantasy.";
+  getTools() {
+    return [];
+  }
+}
+export default SlackPlugin;
